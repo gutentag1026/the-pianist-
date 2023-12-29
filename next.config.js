67963013125af14f6f9a202ca4168b751e20b002
yulhuang <yulhuang@ebay.com>
2023-12-29 15:47:55 +0800

    asd

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+let assetPrefix = ''
+let basePath = '/'
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+
+  assetPrefix = `/${repo}/`
+  basePath = `/${repo}`
+}
 const nextConfig = {
     reactStrictMode: false,
+    assetPrefix: assetPrefix,
+    basePath: basePath,
     env: 
     {
         NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME: "dtljv9iws",
