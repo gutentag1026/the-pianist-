7f72538b66c46c877f15c9794d9d148d3fd2b564
Wendy Huang <huanghuang5087@hotmail.com>
2024-01-11 15:20:22 +0800

    Update PlayList.jsx

--- utils/PlayList.jsx
+++ utils/PlayList.jsx
@@ -1,33 +1,29 @@
 'use client'
+// import cloudinary from '@/utils/cloudinary'
 import { useEffect } from "react";
 
 export default function PlayList({cloudName, mediaAssets}) {
   useEffect(() => {
-   console.log('window playvideo',window)
-   
-        const productGallery = cloudinary.galleryWidget({
-            container: "#my-gallery",
-            cloudName: cloudName,
-            mediaAssets: mediaAssets,
-            aspectRatio: "4:3",
-            borderWidth: 5,
-            border:'black',
-            transition: "fade",
-            sortProps:{ source: "public_id", direction: "desc" },
-            videoProps: {
-                  playerType: "cloudinary",
-                  showJumpControls: true
-                }
-          })
-          productGallery.render();
-    
+    console.log('cloudName',cloudName, 'mediaAssets',mediaAssets)
+    if (window.cloudinary && window.cloudinary.galleryWidget){
+    const productGallery = cloudinary.galleryWidget({
+      container: "#my-gallery",
+      cloudName: cloudName,
+      mediaAssets: mediaAssets,
+      aspectRatio: "4:3",
+      borderWidth: 5,
+      border:'black',
+      transition: "fade",
+      carouselLocation: "bottom",
+      sortProps:{ source: "public_id", direction: "desc" },
+      videoProps:{ controls:"all", sound:true}
+    });
+
+    productGallery.render();
+  }
   }, []);
 
-  return (
-    <div className="container" style={{width:'1000px', margin:'0 10px'}}>
-      <div className="gallery-container">
-        <div id="image-gallery"></div>
-      </div>
-    </div>
-  );
+  return <div style={{width:'1000px', margin:'0 10px'}}>
+            <div id="my-gallery"></div>
+          </div>    
 }
